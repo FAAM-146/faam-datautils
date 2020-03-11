9ba87d0eea158237f72ab57be8720010fd0497f1
Graeme Nott <graeme.nott@faam.ac.uk>
2020-03-11 19:05:02 +0000

    Added start and end special characters to regex strings

--- faamda/wrapper/accessors/core.py
+++ faamda/wrapper/accessors/core.py
@@ -4,9 +4,9 @@ from .accessor import DataAccessor
 @register_accessor
 class CoreAccessor(DataAccessor):
     hook = 'core'
-    regex = ('core_faam_(?P<date>[0-9]{8})_v00(?P<version>[0-9])_'
+    regex = ('^core_faam_(?P<date>[0-9]{8})_v00(?P<version>[0-9])_'
               'r(?P<revision>[0-9]+)_(?P<flightnum>[a-z][0-9]{3})_'
-              '?(?P<freq>[1-9]*)h?z?.nc')
+              '?(?P<freq>[1-9]*)h?z?.nc$')
 
 @register_accessor
 class CoreFltSumAccessor(DataAccessor):
@@ -15,5 +15,5 @@ class CoreFltSumAccessor(DataAccessor):
     .. TODO:: Should extension be an input arg to cope with csv/txt versions?
     """
     hook = 'corefltsum'
-    regex = ('flight-sum_faam_(?P<date>\d{8})_'
-             'r(?P<revision>\d+)_(?P<flightnum>[a-z]\d{3}).csv')
+    regex = ('^flight-sum_faam_(?P<date>\d{8})_'
+             'r(?P<revision>\d+)_(?P<flightnum>[a-z]\d{3}).csv$')
