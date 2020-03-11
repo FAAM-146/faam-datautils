6c7da653525861d34a5f6c3bd2bbeb4a2462b681
Graeme Nott <graeme.nott@faam.ac.uk>
2020-03-11 18:14:28 +0000

    Renamed classes and hooks

--- faamda/wrapper/accessors/corecloud.py
+++ faamda/wrapper/accessors/corecloud.py
@@ -2,57 +2,57 @@ from .register import register_accessor
 from .accessor import DataAccessor
 
 @register_accessor
-class CoreCloudAccessor(DataAccessor):
+class CoreCloudPhysicsAccessor(DataAccessor):
     """Accessor for Core Cloud Physics main nc file
 
     This file contains (as of Mar 2020) PCASP and CDP data
     """
-    hook = 'corecloud'
+    hook = 'ccp'
     regex = ('core-cloud-phy_faam_(?P<date>[0-9]{8})_v(?P<version>[0-9]{3})_'
              'r(?P<revision>[0-9]+)_(?P<flightnum>[a-z][0-9]{3}).nc')
 
 
 @register_accessor
-class CoreCloudCIP15Accessor(DataAccessor):
+class CoreCloudPhysicsCIP15Accessor(DataAccessor):
     """Accessor for Core Cloud Physics CIP15 nc file
 
     .. TODO:: Should all CIPs have same accessor with hook/regex modified
               by input arg?
     """
-    hook = 'corecloudCIP15'
+    hook = 'ccpCIP15'
     regex = ('core-cloud-phy_faam_(?P<date>[0-9]{8})_v(?P<version>[0-9]{3})_'
              'r(?P<revision>[0-9]+)_(?P<flightnum>[a-z][0-9]{3})_cip15.nc')
 
 
 @register_accessor
-class CoreCloudCIP25Accessor(DataAccessor):
+class CoreCloudPhysicsCIP25Accessor(DataAccessor):
     """Accessor for Core Cloud Physics CIP25 nc file
 
     .. TODO:: Should all CIPs have same accessor with hook/regex modified
               by input arg?
     """
-    hook = 'corecloudCIP25'
+    hook = 'ccpCIP25'
     regex = ('core-cloud-phy_faam_(?P<date>[0-9]{8})_v(?P<version>[0-9]{3})_'
              'r(?P<revision>[0-9]+)_(?P<flightnum>[a-z][0-9]{3})_cip25.nc')
 
 
 @register_accessor
-class CoreCloudCIP100Accessor(DataAccessor):
+class CoreCloudPhysicsCIP100Accessor(DataAccessor):
     """Accessor for Core Cloud Physics CIP100 nc file
 
     .. TODO:: Should all CIPs have same accessor with hook/regex modified
               by input arg?
     """
-    hook = 'corecloudCIP100'
+    hook = 'ccpCIP100'
     regex = ('core-cloud-phy_faam_(?P<date>[0-9]{8})_v(?P<version>[0-9]{3})_'
              'r(?P<revision>[0-9]+)_(?P<flightnum>[a-z][0-9]{3})_cip100.nc')
 
 
 @register_accessor
-class CoreCloudCASAccessor(DataAccessor):
+class CoreCloudPhysicsCASAccessor(DataAccessor):
     """Accessor for Core Cloud Physics CAS nc file
 
     """
-    hook = 'corecloudCAS'
+    hook = 'ccpCAS'
     regex = ('core-cloud-phy_faam_(?P<date>[0-9]{8})_v(?P<version>[0-9]{3})_'
              'r(?P<revision>[0-9]+)_(?P<flightnum>[a-z][0-9]{3})_cas.nc')
