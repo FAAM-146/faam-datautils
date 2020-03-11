1cc06ceed90debf0f022961f0db895b80a8cc52c
Graeme Nott <graeme.nott@faam.ac.uk>
2020-03-11 19:06:32 +0000

    Started building netCDF model for xarray

--- faamda/wrapper/models.py
+++ faamda/wrapper/models.py
@@ -1,5 +1,6 @@
 import abc
 from netCDF4 import Dataset
+import xarray as xr
 
 class DataModel(abc.ABC):
     def __init__(self, path):
@@ -40,3 +41,64 @@ class NetCDFDataModel(DataModel):
                 self.time_calendar = nc['Time'].calendar
             except AttributeError:
                 self.time_calendar = None
+
+
+class NetCDFDataModelXR(DataModel):
+    """Returns requested data or metadata from path
+
+    This class uses xarray to return a dataArray or dataset with full
+    attributes as in the netCDF.
+
+    .. TODO:: Could have option arg to return arrays only?
+    """
+
+    def _get_time(self,grp=None):
+        """
+        If grp in [None,'','/'] then look for time in root group
+
+        """
+        try:
+            ds = xr.open_dataset(faam[flight].core.file,group=grp)
+        except OSError as err:
+            # Generally because grp is not a valid file group
+            print(err.errno)
+            #self.time = None # leave undefined?
+        else:
+            with ds:
+                # Find variable with standard_name 'time'. With CF compliant
+                # files this shall cope with different cases of variable name.
+                ds_t = ds.filter_by_attrs(standard_name='time')
+                time_var = list(ds_t.variables)[0]
+                self.time = ds_t[time_var]
+
+
+    def __getitem__(self, item, grp=None):
+        """Reads variable/s from file.
+
+        .. warning::
+
+            Note that it is possible to read variables from different groups
+            and put them into a single dataSet. If the dimensions/coordinates
+            in those groups are different then horrible things may happen.
+
+        Args:
+            var (:obj:`str` or :obj:`list`): Single variable or list of
+                variable strings to read. The variable string/s may include
+                the full path if groups are involved.
+            grp (:obj:`str`): Path to single group, default is None or the
+                file root. The same path is prepended to all items if there
+                are more than one in addition to any path information in the
+                item string/s.
+        """
+
+        if type(item) in [str]:
+            items = [os.path.join(grp, item)]
+        else:
+            items = [os.path.join(grp,i_) for i_ in item]
+
+
+
+        dfs = []
+
+        with Dataset(self.path, 'r') as f:
+            pass
