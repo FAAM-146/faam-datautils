ca516da97fd1700313b616c0bed324cbc09bca9b
Graeme Nott <graeme.nott@faam.ac.uk>
2020-03-14 16:44:21 +0000

    __getitem__ now assisted by _get_attr() and _get_var()

--- faamda/wrapper/models.py
+++ faamda/wrapper/models.py
@@ -109,36 +109,135 @@ class NetCDFDataModelXR(DataModel):
         self.groups = list(_groups)
 
 
-    def __getitem__(self, item, grp=None):
-        """Reads variable/s from file.
+    def _get_attr(self, attr, grp=None):
+        """Returns requested attribute/s from file/group or None if nonexistant.
+
+        """
+
+        try:
+            ds = xr.open_dataset(file, group=grp) # self.file?
+        except OSError as err:
+            # Generally because grp is not a valid file group
+            print(err.errno)
+            return None
+
+        with ds:
+            # discard any attributes that are not in ds
+            ds_attr = {k:ds.attrs[k] for k in attr if k in ds.attrs}
+            if len(ds_attr) == 0:
+                return None
+
+        return ds_attr
+ 
+
+    def _get_var(self, var, grp=None):
+        """Reads requested variable/s from file/group or None if nonexistant.
+
+        """
+
+        try:
+            ds = xr.open_dataset(file, group=grp) # self.file?
+        except OSError as err:
+            # Generally because grp is not a valid file group
+            print(err.errno)
+            return None
+
+        with ds:
+            # discard any variables that are not in ds
+            # this also discards any attributes
+            ds_var = ds[[v for v in var if v in ds]]
+            if len(ds_var.coords) == 0 or len(ds_var.data_vars) == 0:
+                return None
+
+        return ds_var
+
+    
+    def __getitem__(self, item, grp=None, squeeze=True):
+        """Returns item/s from file/group, may be attribute/s or variable/s.
 
         .. warning::
+            Note that requesting both a variable(s) and an attribute(s) does not
+            make any sense. If requesting a variable, a dataset is returned. If
+            requesting an attribute, the value of that attribute is returned. So
+            these two are incompatible. If both variables and attributes are 
+            included in item then the attribute request is discarded.
 
-            Note that it is possible to read variables from different groups
-            and put them into a single dataSet. If the dimensions/coordinates
-            in those groups are different then horrible things may happen.
+        .. note::
+            Variable attributes are returned automatically with the variables.
+            Attributes given in `item` are group (including root) attributes.
 
         Args:
-            var (:obj:`str` or :obj:`list`): Single variable or list of
+            item (:obj:`str` or :obj:`list`): Single variable or list of
                 variable strings to read. The variable string/s may include
                 the full path if groups are involved.
             grp (:obj:`str`): Path to single group, default is None or the
                 file root. The same path is prepended to all items if there
                 are more than one in addition to any path information in the
                 item string/s.
+            squeeze (:obj:`boolean`): If True [default] then returns single
+                dataset with variable/s or None if no variables found. If False
+                then returns list, empty or len==1 in these cases. If more than
+                one dataset is found then list of datasets is always returned.
+
+        Returns:
+            If `squeeze` is False then list of datasets or dictionary of attribute
+                key:value pairs. If `squeeze` is True then, if only single
+                variable or attribute then returns single dataset or attribute
+                value.
         """
+        if grp in [None,'','/']:
+            grp = ''
 
         if type(item) in [str]:
             items = [os.path.join(grp, item)]
         else:
-            items = [os.path.join(grp,i_) for i_ in item]
+            items = [os.path.join(grp, i) for i in item]
+        del grp
+        
+        # Get list of unique groups. Unfortunately open_dataset must be called
+        # for each different group.
+        _grps, _items = zip(*sorted([os.path.split(i_) for i_ in items],
+                                    key=lambda g: g[0]))
+
+        _grps = [g.replace('/','') for g in _grps[::]]
+        _grps_uniq = set(_grps)
+        _grps_idx = [[i for i,x in enumerate(_grps)  if x==y] for y in _grps_uniq]
+
+
+        iteml = []
+        for idx, grp in zip(_grps_idx, _grps_uniq):
+            # Loop through each group and pass all of the items from that group
+            iteml.append(self._get_var([_items[i] for i in idx],
+                                       grp))
+
+        if len(iteml) == 0:
+            # No variables found. Check for attributes
+
+            ### TODO:: This is not a very good way of doing this...
+            
+            for idx, grp in zip(_grps_idx, _grps_uniq):
+                # Loop through each group and pass all of the items from that group
+                iteml.append(self._get_attr([_items[i] for i in idx],
+                                            grp))
+
+            if squeeze and len(iteml) == 0:
+                return None
+            if squeeze and len(iteml) == 1:
+                # Return only value of single attribute found
+                # squeeze should not be used if multiple unknown attribute keys
+                # are passed in as a single returned value could belong to any...
+                return iteml[iteml.keys()[0]]
+
+        else:
+            # Returning variable/s
+            if squeeze and len(iteml) == 1:
+                # If only single dataset then extract from list and return
+                return iteml[0]
+        
+        return iteml
 
 
 
-        dfs = []
-
-        with Dataset(self.path, 'r') as f:
-            pass
 
 
     @property
@@ -171,3 +270,9 @@ class NetCDFDataModelXR(DataModel):
         """
 
         # haven't done this yet
+        def walktree(top):
+            values = top.groups.values()
+            yield values
+            for value in top.groups.values():
+                for children in walktree(value):
+                    yield children
